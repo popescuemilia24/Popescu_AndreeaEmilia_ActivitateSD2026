7163546d47bce80c4eceea41c32c30d54a080c1d
Andreea Popescu <popescuemilia2005@gmail.com>
2026-03-05 09:09:48 +0200

    Exercitiu seminar1 - Bilet

--- Seminar01.c
+++ Seminar01.c
@@ -73,3 +73,59 @@
 //	dezalocare(&s);
 //	return 0;
 //}
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct Bilet {
+	int id;
+	int nr_loc;
+	char* eveniment;
+	float pret;
+	char rand;
+};
+
+struct Bilet initializare(int id, int nr_loc, char* eveniment, float pret, char rand) {
+	struct Bilet s;
+	s.id = id;
+	s.nr_loc = nr_loc;
+	s.eveniment = (char*)malloc(sizeof(char) * (strlen(eveniment) + 1));
+	if (s.eveniment != NULL) {
+		strcpy_s(s.eveniment, strlen(eveniment) + 1, eveniment);
+	}
+	s.pret = pret;
+	s.rand = rand;
+	return s;
+}
+
+void afisare(struct Bilet s) {
+	printf("Bilet ID: %d, Eveniment: %s, Loc: %d, Rand: %c, Pret: %.2f RON\n", s.id, s.eveniment, s.nr_loc, s.rand, s.pret);
+}
+
+void modifica_Eveniment(struct Bilet* s, char* evenimentNou) {
+	free((*s).eveniment);
+	(*s).eveniment = (char*)malloc(sizeof(char) * (strlen(evenimentNou) + 1));
+	strcpy_s(s->eveniment, strlen(evenimentNou) + 1, evenimentNou);
+}
+
+void dezalocare(struct Bilet* s) {
+	if (s->eveniment != NULL)
+	{
+		free(s->eveniment);
+		s->eveniment = NULL;
+	}
+}
+
+int main() {
+	struct Bilet s;
+
+	s = initializare(1, 25, "Concert Rock", 150.5, 'F');
+	afisare(s);
+
+	modifica_Eveniment(&s, "Festival Jazz");
+	afisare(s);
+
+	dezalocare(&s);
+	return 0;
+}
--- Seminar02.c
+++ Seminar02.c
@@ -1,138 +1,138 @@
-// [] indexare = deplasare + dereferentiere
-// -> = dereferentiere + accesare
-
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-
-
-
-struct Biblioteca {
-	int id;
-	int nrCarti;
-	char* nume;
-	float suprafata;
-	char sector;
-};
-struct Biblioteca initializare(int id, int nrCarti, char* nume, float suprafata, char sector) {
-	struct Biblioteca s;
-	s.id = id;
-	s.nrCarti = nrCarti;
-	s.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
-	strcpy(s.nume, nume);
-	s.suprafata = suprafata;
-	s.sector = sector;
-	return s;
-}
-
-struct Biblioteca copiazaBiblioteca(struct Biblioteca sursa) {
-	return initializare(sursa.id, sursa.nrCarti, sursa.nume, sursa.suprafata, sursa.sector);
-
-}
-
-void afisare(struct Biblioteca s) {
-	printf("ID-ul: %d\t\n Numarul de carti: %d\n Denumire: %s\n Suprafata: %5.2f\n Sector: %c\n", s.id, s.nrCarti, s.nume, s.suprafata, s.sector);
-}
-
-void afisareVector(struct Biblioteca* vector, int nrElemente) {
-	for (int i = 0; i < nrElemente; i++) {
-		afisare(vector[i]);
-	}
-}
-
-struct Biblioteca* copiazaPrimeleNElemente(struct Biblioteca* vector, int nrElemente, int nrElementeCopiate) {
-
-	struct Biblioteca* vectorNou = NULL;
-	if (nrElementeCopiate > nrElemente) {
-		nrElementeCopiate = nrElemente;
-	}
-
-	vectorNou = (struct Biblioteca*)malloc(sizeof(struct Biblioteca) * nrElementeCopiate);
-	for (int i = 0; i < nrElementeCopiate; i++) {
-		vectorNou[i] = copiazaBiblioteca(vector[i]);
-	}
-
-	return vectorNou;
-}
-
-void dezalocare(struct Biblioteca** vector, int* nrElemente) {
-
-	for (int i = 0; i < *nrElemente; ++i) {
-		free((*vector)[i].nume);
-
-
-	}
-	free((*vector));
-	*vector = NULL;
-	*nrElemente = 0;
-
-
-}
-
-void copiazaBibcuSupMare(struct Biblioteca* vector, char nrElemente, float supMin, struct Biblioteca** vectorNou, int* dimensiune) {
-
-	*dimensiune = 0;
-	for (int i = 0; i < nrElemente; ++i) {
-		if (vector[i].suprafata > supMin) {
-			(*dimensiune)++;
-		}
-	}
-
-	*vectorNou = (struct Biblioteca*)malloc(sizeof(struct Biblioteca) * (*dimensiune));
-
-	int contor = 0;
-	for (int i = 0; i < nrElemente; ++i) {
-		if (vector[i].suprafata > supMin) {
-			(*vectorNou)[contor++] = copiazaBiblioteca(vector[i]);
-		}
-	}
-}
-
-struct Biblioteca getPrimulElementConditionat(struct Biblioteca* vector, int nrElemente, const char* conditie) {
-
-	struct Biblioteca s;
-	s.id = 1;
-	
-	return s;
-}
-
-
-
-int main() {
-	struct Biblioteca b;
-	b = initializare(1, 100, "Biblioteca nationala", 500.5, '3');
-	//afisare(b);
-
-	int numarBiblioteci = 5;
-	struct Biblioteca* vectorBiblioteci = (struct Biblioteca*)malloc(sizeof(struct Biblioteca) * numarBiblioteci);
-
-	//o biblioteca ocupa 21 de octeti, iar malloc aloca 40 octeti => acum ocupa 205 octeti
-	vectorBiblioteci[0] = copiazaBiblioteca(b);
-	vectorBiblioteci[1] = initializare(2, 200, "Biblioteca ASE", 200.6, '1');
-	vectorBiblioteci[2] = initializare(3, 300, "Biblioteca Sadoveanu", 234.8, '4');
-	vectorBiblioteci[3] = initializare(4, 400, "Biblioteca Creanga", 345.6, '5');
-	vectorBiblioteci[4] = initializare(5, 500, "Biblioteca I.L.Caragiale", 456.9, '6');
-
-	afisareVector(vectorBiblioteci, numarBiblioteci);
-	int nrCopiate = 3;
-	struct Biblioteca* copie = copiazaPrimeleNElemente(vectorBiblioteci, numarBiblioteci, nrCopiate);
-	printf("\nElementele vect cu primele %d elemente copiate\n", nrCopiate);
-	afisareVector(copie, nrCopiate);
-
-	dezalocare(&copie, &nrCopiate);
-
-	struct Biblioteca* vBibcuSupMAre = NULL;
-	int nrBibCuSupM = 0;
-	copiazaBibcuSupMare(vectorBiblioteci, numarBiblioteci, 300.00, &vBibcuSupMAre, &nrBibCuSupM);
-	printf("\nElementele vect biblioteci cu suprafata mare\n");
-	afisareVector(vBibcuSupMAre, nrBibCuSupM);
-
-	dezalocare(&vBibcuSupMAre, &nrBibCuSupM);
-	dezalocare(&vectorBiblioteci, &numarBiblioteci);
-
-	
-	return 0;
-}
-
+//// [] indexare = deplasare + dereferentiere
+//// -> = dereferentiere + accesare
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//
+//
+//
+//
+//struct Biblioteca {
+//	int id;
+//	int nrCarti;
+//	char* nume;
+//	float suprafata;
+//	char sector;
+//};
+//struct Biblioteca initializare(int id, int nrCarti, char* nume, float suprafata, char sector) {
+//	struct Biblioteca s;
+//	s.id = id;
+//	s.nrCarti = nrCarti;
+//	s.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+//	strcpy(s.nume, nume);
+//	s.suprafata = suprafata;
+//	s.sector = sector;
+//	return s;
+//}
+//
+//struct Biblioteca copiazaBiblioteca(struct Biblioteca sursa) {
+//	return initializare(sursa.id, sursa.nrCarti, sursa.nume, sursa.suprafata, sursa.sector);
+//
+//}
+//
+//void afisare(struct Biblioteca s) {
+//	printf("ID-ul: %d\t\n Numarul de carti: %d\n Denumire: %s\n Suprafata: %5.2f\n Sector: %c\n", s.id, s.nrCarti, s.nume, s.suprafata, s.sector);
+//}
+//
+//void afisareVector(struct Biblioteca* vector, int nrElemente) {
+//	for (int i = 0; i < nrElemente; i++) {
+//		afisare(vector[i]);
+//	}
+//}
+//
+//struct Biblioteca* copiazaPrimeleNElemente(struct Biblioteca* vector, int nrElemente, int nrElementeCopiate) {
+//
+//	struct Biblioteca* vectorNou = NULL;
+//	if (nrElementeCopiate > nrElemente) {
+//		nrElementeCopiate = nrElemente;
+//	}
+//
+//	vectorNou = (struct Biblioteca*)malloc(sizeof(struct Biblioteca) * nrElementeCopiate);
+//	for (int i = 0; i < nrElementeCopiate; i++) {
+//		vectorNou[i] = copiazaBiblioteca(vector[i]);
+//	}
+//
+//	return vectorNou;
+//}
+//
+//void dezalocare(struct Biblioteca** vector, int* nrElemente) {
+//
+//	for (int i = 0; i < *nrElemente; ++i) {
+//		free((*vector)[i].nume);
+//
+//
+//	}
+//	free((*vector));
+//	*vector = NULL;
+//	*nrElemente = 0;
+//
+//
+//}
+//
+//void copiazaBibcuSupMare(struct Biblioteca* vector, char nrElemente, float supMin, struct Biblioteca** vectorNou, int* dimensiune) {
+//
+//	*dimensiune = 0;
+//	for (int i = 0; i < nrElemente; ++i) {
+//		if (vector[i].suprafata > supMin) {
+//			(*dimensiune)++;
+//		}
+//	}
+//
+//	*vectorNou = (struct Biblioteca*)malloc(sizeof(struct Biblioteca) * (*dimensiune));
+//
+//	int contor = 0;
+//	for (int i = 0; i < nrElemente; ++i) {
+//		if (vector[i].suprafata > supMin) {
+//			(*vectorNou)[contor++] = copiazaBiblioteca(vector[i]);
+//		}
+//	}
+//}
+//
+//struct Biblioteca getPrimulElementConditionat(struct Biblioteca* vector, int nrElemente, const char* conditie) {
+//
+//	struct Biblioteca s;
+//	s.id = 1;
+//	
+//	return s;
+//}
+//
+//
+//
+//int main() {
+//	struct Biblioteca b;
+//	b = initializare(1, 100, "Biblioteca nationala", 500.5, '3');
+//	//afisare(b);
+//
+//	int numarBiblioteci = 5;
+//	struct Biblioteca* vectorBiblioteci = (struct Biblioteca*)malloc(sizeof(struct Biblioteca) * numarBiblioteci);
+//
+//	//o biblioteca ocupa 21 de octeti, iar malloc aloca 40 octeti => acum ocupa 205 octeti
+//	vectorBiblioteci[0] = copiazaBiblioteca(b);
+//	vectorBiblioteci[1] = initializare(2, 200, "Biblioteca ASE", 200.6, '1');
+//	vectorBiblioteci[2] = initializare(3, 300, "Biblioteca Sadoveanu", 234.8, '4');
+//	vectorBiblioteci[3] = initializare(4, 400, "Biblioteca Creanga", 345.6, '5');
+//	vectorBiblioteci[4] = initializare(5, 500, "Biblioteca I.L.Caragiale", 456.9, '6');
+//
+//	afisareVector(vectorBiblioteci, numarBiblioteci);
+//	int nrCopiate = 3;
+//	struct Biblioteca* copie = copiazaPrimeleNElemente(vectorBiblioteci, numarBiblioteci, nrCopiate);
+//	printf("\nElementele vect cu primele %d elemente copiate\n", nrCopiate);
+//	afisareVector(copie, nrCopiate);
+//
+//	dezalocare(&copie, &nrCopiate);
+//
+//	struct Biblioteca* vBibcuSupMAre = NULL;
+//	int nrBibCuSupM = 0;
+//	copiazaBibcuSupMare(vectorBiblioteci, numarBiblioteci, 300.00, &vBibcuSupMAre, &nrBibCuSupM);
+//	printf("\nElementele vect biblioteci cu suprafata mare\n");
+//	afisareVector(vBibcuSupMAre, nrBibCuSupM);
+//
+//	dezalocare(&vBibcuSupMAre, &nrBibCuSupM);
+//	dezalocare(&vectorBiblioteci, &numarBiblioteci);
+//
+//	
+//	return 0;
+//}
+//
