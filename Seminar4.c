9262bd3d374cfae2382d43cb05571eb5c5d48a9e
Andreea Popescu <popescuemilia2005@gmail.com>
2026-03-18 01:08:29 +0200

    comentariu exercitiu - Zbor (LSI)

--- Seminar4.c
+++ Seminar4.c
@@ -196,215 +196,215 @@
 
 
 
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-struct StructuraZbor {
-	int id;
-	int nrPasageri;
-	float durata;
-	char* aeroportPlecare;
-	char* aeroportSosire;
-	unsigned char status; // 0=la timp, 1=întârziat, 2=anulat
-};
-typedef struct StructuraZbor Zbor;
-
-typedef struct Nod Nod;
-struct Nod
-{
-	Zbor info;
-	Nod* next;
-};
-
-
-
-Zbor citireZborFisier(FILE* file) {
-	Zbor z;
-	char buffer[100];
-	char sep[3] = ",\n";
-	fgets(buffer, 100, file);
-	z.id = atoi(strtok(buffer, sep));
-	z.nrPasageri = atoi(strtok(NULL, sep));
-	z.durata = atof(strtok(NULL, sep));
-	char* aux = strtok(NULL, sep);
-	z.aeroportPlecare = (char*)malloc(sizeof(char) * (strlen(aux) + 1));
-	strcpy_s(z.aeroportPlecare, strlen(aux) + 1, aux);
-	aux = strtok(NULL, sep);
-	z.aeroportSosire = (char*)malloc(sizeof(char) * (strlen(aux) + 1));
-	strcpy_s(z.aeroportSosire, strlen(aux) + 1, aux);
-	z.status = (strtok(NULL, sep))[0];
-	return z;
-}
-
-void afisareZbor(Zbor zbor) {
-	printf("ID: %d\n", zbor.id);
-	printf("PASAGERI: %d\n", zbor.nrPasageri);
-	printf("DURATA: %.2f\n", zbor.durata);
-	printf("PLECARE: %s\n", zbor.aeroportPlecare);
-	printf("SOSIRE: %s\n", zbor.aeroportSosire);
-	printf("STATUS: %c\n\n", zbor.status);
-}
-
-void afisareListaZbor(Nod* cap) {
-	while (cap != NULL)
-	{
-		afisareZbor(cap->info);
-		cap = cap->next;
-	}
-}
-
-void adaugaZborInLista(Nod** cap, Zbor ZborNou) {
-	Nod* nou = (Nod*)malloc(sizeof(Nod));
-
-	nou->info = ZborNou;
-	nou->next = NULL;
-
-	if (*cap == NULL)
-	{
-		(*cap) = nou;
-	}
-	else {
-		Nod* c = *cap;
-		while (c->next != NULL)
-		{
-			c = c->next;
-		}
-		c->next = nou;
-	}
-
-}
-
-void adaugaLaInceputInLista(Nod** cap, Zbor ZborNou) {
-	Nod* nou = (Nod*)malloc(sizeof(Nod));
-	nou->info = ZborNou;
-	nou->next = *cap;
-	*cap = nou;
-
-}
-
-Nod* citireListaZborDinFisier(const char* numeFisier) {
-	FILE* f = fopen(numeFisier, "r");
-
-	if (f)
-	{
-		Nod* cap = NULL;
-		while (!feof(f))
-		{
-			adaugaZborInLista(&cap, citireZborFisier(f));
-		}
-		fclose(f);
-		return cap;
-	}
-	return NULL;
-}
-
-void dezalocareListaZbor(Nod** cap) {
-	while (*cap)
-	{
-		Nod* c = *cap;
-		(*cap) = c->next;
-		if (c->info.aeroportPlecare)
-		{
-			free(c->info.aeroportPlecare);
-		}
-		if (c->info.aeroportSosire)
-		{
-			free(c->info.aeroportSosire);
-		}
-		free(c);
-
-	}
-}
-
-float calculeazaDurataMedie(Nod* cap) {
-	float sum = 0;
-	int k = 0;
-
-	while (cap)
-	{
-		sum += cap->info.durata;
-		k++;
-		cap = cap->next;
-	}
-	if (k != 0)
-	{
-		return sum / k;
-	}
-
-	return 0;
-}
-
-void stergeZborCuStatus(Nod** cap, char statusCautat) {
-	while ((*cap) && (*cap)->info.status == statusCautat) {
-		Nod* c = *cap;
-		(*cap) = c->next;
-		if (c->info.aeroportPlecare)
-			free(c->info.aeroportPlecare);
-		if (c->info.aeroportSosire)
-			free(c->info.aeroportSosire);
-	}
-
-	Nod* c = *cap;
-	while (c) {
-		while (c->next && c->next->info.status != statusCautat) {
-			c = c->next;
-		}
-
-		if (c->next) {
-			Nod* temp = c->next;
-			c->next = temp->next;
-
-			if (temp->info.aeroportSosire) {
-				free(temp->info.aeroportSosire);
-			}
-			if (temp->info.aeroportPlecare)
-				free(temp->info.aeroportPlecare);
-			free(temp);
-		}
-		else {
-			c = c->next;
-		}
-	}
-}
-
-float calculeazaDurataTotalaSpreAeroport(Nod* cap, const char* aeroportSosire) {
-	float total = 0;
-	while (cap != NULL) {
-		if (strcmp(cap->info.aeroportSosire, aeroportSosire) == 0) {
-			total += cap->info.durata;
-		}
-		cap = cap->next;
-	}
-
-	return total;
-}
-
-int main() {
-
-	Nod* cap = NULL;
-	cap = citireListaZborDinFisier("zbor.txt");
-	afisareListaZbor(cap);
-
-
-
-	printf("Durata medie este: %5.2f\n\n", calculeazaDurataMedie(cap));
-
-	const char* destinatieCautata = "Tokyo";
-	printf("Durata totala spre aeroportul %s este: %5.2f\n\n", destinatieCautata, calculeazaDurataTotalaSpreAeroport(cap, destinatieCautata));
-
-
-	printf("Stergere starea 1\n");
-	stergeZborCuStatus(&cap, '1');
-
-	printf("\nLista dupa stegre status 1:\n");
-	afisareListaZbor(cap);
-
-	dezalocareListaZbor(&cap);
-	printf("\nLista dupa dezalocare\n");
-	afisareListaZbor(cap);
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//
+//struct StructuraZbor {
+//	int id;
+//	int nrPasageri;
+//	float durata;
+//	char* aeroportPlecare;
+//	char* aeroportSosire;
+//	unsigned char status; // 0=la timp, 1=întârziat, 2=anulat
+//};
+//typedef struct StructuraZbor Zbor;
+//
+//typedef struct Nod Nod;
+//struct Nod
+//{
+//	Zbor info;
+//	Nod* next;
+//};
+//
+//
+//
+//Zbor citireZborFisier(FILE* file) {
+//	Zbor z;
+//	char buffer[100];
+//	char sep[3] = ",\n";
+//	fgets(buffer, 100, file);
+//	z.id = atoi(strtok(buffer, sep));
+//	z.nrPasageri = atoi(strtok(NULL, sep));
+//	z.durata = atof(strtok(NULL, sep));
+//	char* aux = strtok(NULL, sep);
+//	z.aeroportPlecare = (char*)malloc(sizeof(char) * (strlen(aux) + 1));
+//	strcpy_s(z.aeroportPlecare, strlen(aux) + 1, aux);
+//	aux = strtok(NULL, sep);
+//	z.aeroportSosire = (char*)malloc(sizeof(char) * (strlen(aux) + 1));
+//	strcpy_s(z.aeroportSosire, strlen(aux) + 1, aux);
+//	z.status = (strtok(NULL, sep))[0];
+//	return z;
+//}
+//
+//void afisareZbor(Zbor zbor) {
+//	printf("ID: %d\n", zbor.id);
+//	printf("PASAGERI: %d\n", zbor.nrPasageri);
+//	printf("DURATA: %.2f\n", zbor.durata);
+//	printf("PLECARE: %s\n", zbor.aeroportPlecare);
+//	printf("SOSIRE: %s\n", zbor.aeroportSosire);
+//	printf("STATUS: %c\n\n", zbor.status);
+//}
+//
+//void afisareListaZbor(Nod* cap) {
+//	while (cap != NULL)
+//	{
+//		afisareZbor(cap->info);
+//		cap = cap->next;
+//	}
+//}
+//
+//void adaugaZborInLista(Nod** cap, Zbor ZborNou) {
+//	Nod* nou = (Nod*)malloc(sizeof(Nod));
+//
+//	nou->info = ZborNou;
+//	nou->next = NULL;
+//
+//	if (*cap == NULL)
+//	{
+//		(*cap) = nou;
+//	}
+//	else {
+//		Nod* c = *cap;
+//		while (c->next != NULL)
+//		{
+//			c = c->next;
+//		}
+//		c->next = nou;
+//	}
+//
+//}
+//
+//void adaugaLaInceputInLista(Nod** cap, Zbor ZborNou) {
+//	Nod* nou = (Nod*)malloc(sizeof(Nod));
+//	nou->info = ZborNou;
+//	nou->next = *cap;
+//	*cap = nou;
+//
+//}
+//
+//Nod* citireListaZborDinFisier(const char* numeFisier) {
+//	FILE* f = fopen(numeFisier, "r");
+//
+//	if (f)
+//	{
+//		Nod* cap = NULL;
+//		while (!feof(f))
+//		{
+//			adaugaZborInLista(&cap, citireZborFisier(f));
+//		}
+//		fclose(f);
+//		return cap;
+//	}
+//	return NULL;
+//}
+//
+//void dezalocareListaZbor(Nod** cap) {
+//	while (*cap)
+//	{
+//		Nod* c = *cap;
+//		(*cap) = c->next;
+//		if (c->info.aeroportPlecare)
+//		{
+//			free(c->info.aeroportPlecare);
+//		}
+//		if (c->info.aeroportSosire)
+//		{
+//			free(c->info.aeroportSosire);
+//		}
+//		free(c);
+//
+//	}
+//}
+//
+//float calculeazaDurataMedie(Nod* cap) {
+//	float sum = 0;
+//	int k = 0;
+//
+//	while (cap)
+//	{
+//		sum += cap->info.durata;
+//		k++;
+//		cap = cap->next;
+//	}
+//	if (k != 0)
+//	{
+//		return sum / k;
+//	}
+//
+//	return 0;
+//}
+//
+//void stergeZborCuStatus(Nod** cap, char statusCautat) {
+//	while ((*cap) && (*cap)->info.status == statusCautat) {
+//		Nod* c = *cap;
+//		(*cap) = c->next;
+//		if (c->info.aeroportPlecare)
+//			free(c->info.aeroportPlecare);
+//		if (c->info.aeroportSosire)
+//			free(c->info.aeroportSosire);
+//	}
+//
+//	Nod* c = *cap;
+//	while (c) {
+//		while (c->next && c->next->info.status != statusCautat) {
+//			c = c->next;
+//		}
+//
+//		if (c->next) {
+//			Nod* temp = c->next;
+//			c->next = temp->next;
+//
+//			if (temp->info.aeroportSosire) {
+//				free(temp->info.aeroportSosire);
+//			}
+//			if (temp->info.aeroportPlecare)
+//				free(temp->info.aeroportPlecare);
+//			free(temp);
+//		}
+//		else {
+//			c = c->next;
+//		}
+//	}
+//}
+//
+//float calculeazaDurataTotalaSpreAeroport(Nod* cap, const char* aeroportSosire) {
+//	float total = 0;
+//	while (cap != NULL) {
+//		if (strcmp(cap->info.aeroportSosire, aeroportSosire) == 0) {
+//			total += cap->info.durata;
+//		}
+//		cap = cap->next;
+//	}
+//
+//	return total;
+//}
+//
+//int main() {
+//
+//	Nod* cap = NULL;
+//	cap = citireListaZborDinFisier("zbor.txt");
+//	afisareListaZbor(cap);
+//
+//
+//
+//	printf("Durata medie este: %5.2f\n\n", calculeazaDurataMedie(cap));
+//
+//	const char* destinatieCautata = "Tokyo";
+//	printf("Durata totala spre aeroportul %s este: %5.2f\n\n", destinatieCautata, calculeazaDurataTotalaSpreAeroport(cap, destinatieCautata));
+//
+//
+//	printf("Stergere starea 1\n");
+//	stergeZborCuStatus(&cap, '1');
+//
+//	printf("\nLista dupa stegre status 1:\n");
+//	afisareListaZbor(cap);
+//
+//	dezalocareListaZbor(&cap);
+//	printf("\nLista dupa dezalocare\n");
+//	afisareListaZbor(cap);
+//
+//	return 0;
+//}
