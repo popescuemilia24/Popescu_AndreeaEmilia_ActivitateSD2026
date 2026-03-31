73031d6b3f9b9ad8198aa0db1b1f1fcf35f4b81d
Andreea Popescu <popescuemilia2005@gmail.com>
2026-03-31 15:44:28 +0300

    Seminar 7

---
+++ Seminar07.c
@@ -0,0 +1,234 @@
+////stiva = structura liniara, omogena, LIFO
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+////STIVA
+//
+//struct StructuraMasina {
+//	int id;
+//	int nrUsi;
+//	float pret;
+//	char* model;
+//	char* numeSofer;
+//	unsigned char serie;
+//};
+//typedef struct StructuraMasina Masina;
+//
+//Masina citireMasinaDinFisier(FILE* file) {
+//	char buffer[100];
+//	char sep[3] = ",\n";
+//	fgets(buffer, 100, file);
+//	char* aux;
+//	Masina m1;
+//	aux = strtok(buffer, sep);
+//	m1.id = atoi(aux);
+//	m1.nrUsi = atoi(strtok(NULL, sep));
+//	m1.pret = atof(strtok(NULL, sep));
+//	aux = strtok(NULL, sep);
+//	m1.model = malloc(strlen(aux) + 1);
+//	strcpy_s(m1.model, strlen(aux) + 1, aux);
+//
+//	aux = strtok(NULL, sep);
+//	m1.numeSofer = malloc(strlen(aux) + 1);
+//	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+//
+//	m1.serie = *strtok(NULL, sep);
+//	return m1;
+//}
+//
+//void afisareMasina(Masina masina) {
+//	printf("Id: %d\n", masina.id);
+//	printf("Nr. usi : %d\n", masina.nrUsi);
+//	printf("Pret: %.2f\n", masina.pret);
+//	printf("Model: %s\n", masina.model);
+//	printf("Nume sofer: %s\n", masina.numeSofer);
+//	printf("Serie: %c\n\n", masina.serie);
+//}
+//
+//typedef struct Nod Nod;
+//struct Nod {
+//	Nod* next;
+//	Masina info;
+//};
+//
+//void pushStack(Nod** prim, Masina masina) {
+//	Nod* nou = malloc(sizeof(Nod));
+//	nou->info = masina;
+//	nou->next = (*prim);
+//	(*prim) = nou;
+//}
+//
+//Masina popStack(Nod** prim) {
+//	if (prim != NULL)
+//	{
+//		Masina rezultat = (*prim)->info; //sageata are pioritate mai mare decat dereferentierea
+//		Nod* aux = (*prim);
+//		(*prim) = (*prim)->next;
+//		free(aux);
+//		return rezultat;
+//	}
+//	else {
+//		Masina nou;
+//		nou.id = -1;
+//		return nou;
+//	}
+//
+//}
+//
+//int emptyStack(/*stiva*/) {
+//	//verificare stiva goala
+//}
+//
+//Nod* citireStackMasiniDinFisier(const char* numeFisier) {
+//	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+//	//prin apelul repetat al functiei citireMasinaDinFisier()
+//	//ATENTIE - la final inchidem fisierul/stream-ul
+//
+//	FILE* f = fopen(numeFisier, "r");
+//	Nod* stiva;
+//	stiva = NULL;
+//	while (!feof(f))
+//	{
+//		pushStack(&stiva, citireMasinaDinFisier(f));
+//	}
+//	fclose(f);
+//	return stiva;
+//}
+//
+//void dezalocareStivaDeMasini(/*stiva*/) {
+//	//sunt dezalocate toate masinile si stiva de elemente
+//}
+//
+//int size(/*stiva*/) {
+//	//returneaza numarul de elemente din stiva
+//}
+//
+////QUEUE
+//
+//typedef struct NodD NodD;
+//struct NodD {
+//	Masina info;
+//	NodD* prev;
+//	NodD* next;
+//};
+//
+//typedef struct ListaDubla ListaDubla;
+//struct ListaDubla {
+//	NodD* first;
+//	NodD* last;
+//};
+//
+////adaugare la inceput
+//void enqueue(ListaDubla* lista, Masina masina) {
+//	NodD* nou = (NodD*)malloc(sizeof(NodD));
+//	nou->info = masina;
+//	nou->next = lista->first;
+//	nou->prev = NULL;
+//	if (lista->first)
+//	{
+//		lista->first->prev = nou;
+//	}
+//	else {
+//		lista->last = nou;
+//	}
+//	lista->first = nou;
+//}
+//
+//Masina dequeue(ListaDubla* lista) {
+//	Masina noua;
+//
+//	if (lista->last) {
+//		noua = lista->last->info;
+//		lista->last = lista->last->prev;
+//		if (lista->last)
+//		{
+//			free(lista->last->next);
+//			lista->last->next = NULL;
+//		}
+//		else {
+//			free(lista->first);
+//			lista->first = NULL;
+//		}
+//
+//	}
+//	else {
+//		noua.id = -1;
+//	}
+//	return noua;
+//}
+//
+//ListaDubla citireCoadaDeMasiniDinFisier(const char* numeFisier) {
+//	ListaDubla coada;
+//	coada.first = coada.last = NULL;
+//	FILE* f = fopen(numeFisier, "r");
+//	if (f) {
+//		while (!feof(f))
+//		{
+//			enqueue(&coada, citireMasinaDinFisier(f));
+//		}
+//		fclose(f);
+//	}
+//	return coada;
+//}
+//
+//void dezalocareCoadaDeMasini(/*coada*/) {
+//	//sunt dezalocate toate masinile si coada de elemente
+//}
+//
+//
+////metode de procesare
+//Masina getMasinaByID(Nod** prim, int id) {
+//	Masina aux;
+//	Masina rez;
+//	Nod* stivaNoua = NULL;
+//
+//	while ((*prim) && (*prim)->info.id != id) {
+//		pushStack(&stivaNoua, popStack(prim));
+//	}
+//	if (*prim) {
+//		aux = popStack(prim);
+//
+//		rez = aux;
+//		rez.model = (char*)malloc(sizeof(char) * (strlen(aux.model) + 1));
+//		strcpy(rez.model, aux.model);
+//
+//		rez.numeSofer = (char*)malloc(sizeof(char) * (strlen(aux.numeSofer) + 1));
+//		strcpy(rez.numeSofer, aux.numeSofer);
+//
+//		pushStack(prim, aux);
+//	}
+//	else {
+//		rez.id = -1;
+//
+//	}
+//
+//	while (stivaNoua) {
+//		pushStack(prim, popStack(&stivaNoua));
+//	}
+//	return rez;
+//
+//}
+//
+//float calculeazaPretTotal(/*stiva sau coada de masini*/);
+//
+//int main() {
+//
+//	Nod* stiva = citireStackMasiniDinFisier("masini.txt");
+//	afisareMasina(popStack(&stiva));
+//	afisareMasina(popStack(&stiva));
+//
+//	afisareMasina(getMasinaByID(&stiva, 3));
+//
+//
+//
+//	ListaDubla coada = citireCoadaDeMasiniDinFisier("masini.txt");
+//	printf("Coada:\n");
+//	afisareMasina(dequeue(&coada));
+//	afisareMasina(dequeue(&coada));
+//
+//
+//	return 0;
+//}
